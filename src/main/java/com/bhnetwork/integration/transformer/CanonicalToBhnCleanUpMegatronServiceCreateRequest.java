66c7b1c594182610044053e383e841fad1687012
Raja Venugopal <raja.venugopal@bhnetwork.com>
2014-05-20 10:16:26 -0700

    Cleanup Service Integration.

--- src/main/java/com/bhnetwork/integration/transformer/CanonicalToBhnCleanUpMegatronServiceCreateRequest.java
+++ src/main/java/com/bhnetwork/integration/transformer/CanonicalToBhnCleanUpMegatronServiceCreateRequest.java
@@ -2,7 +2,12 @@ package com.bhnetwork.integration.transformer;
 
 import org.mule.api.MuleMessage;
 import org.mule.api.transformer.TransformerException;
+import org.mule.api.transport.PropertyScope;
 import org.mule.transformer.AbstractMessageTransformer;
+import org.tempuri.BhnCleanUpMegatronServiceCreateRequest;
+
+import com.microsoft.schemas.dynamics._2008._01.documents.bhncleanupmegatron.AxdEntityBhnCleanUpMegatron;
+import com.microsoft.schemas.dynamics._2008._01.documents.bhncleanupmegatron.AxdbhnCleanUpMegatron;
 
 public class CanonicalToBhnCleanUpMegatronServiceCreateRequest extends
 		AbstractMessageTransformer {
@@ -10,7 +15,21 @@ public class CanonicalToBhnCleanUpMegatronServiceCreateRequest extends
 	@Override
 	public Object transformMessage(MuleMessage message, String outputEncoding)
 			throws TransformerException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		BhnCleanUpMegatronServiceCreateRequest req = new BhnCleanUpMegatronServiceCreateRequest();
+		String partnerProfileId = message.getProperty("partnerProfileId", PropertyScope.SESSION).toString();
+		
+		AxdbhnCleanUpMegatron bhnCleanUpMegatron = new AxdbhnCleanUpMegatron();
+		
+		AxdEntityBhnCleanUpMegatron entityBhnCleanUpMegatron = new AxdEntityBhnCleanUpMegatron();
+		entityBhnCleanUpMegatron.setPartnerprofileId(partnerProfileId);
+		//XML Attribute required
+		entityBhnCleanUpMegatron.setClazz("entity");//TODO Not in SPEC
+		
+		bhnCleanUpMegatron.getBhnCleanUpMegatron().add(entityBhnCleanUpMegatron);
+		
+		req.setBhnCleanUpMegatron(bhnCleanUpMegatron);
+		
+		return req;
 	}
 }
