0268e45e866e7eb86e0f5b4761dc9bfedbc898e4
Raja Venugopal <raja.venugopal@bhnetwork.com>
2014-05-27 07:16:18 -0700

    Dax flow step 2, 3, 4, 5

--- src/main/java/com/bhnetwork/integration/transformer/CanonicalToBhnDivision3PSServiceCreateRequest.java
+++ src/main/java/com/bhnetwork/integration/transformer/CanonicalToBhnDivision3PSServiceCreateRequest.java
@@ -4,13 +4,19 @@ import org.mule.api.MuleMessage;
 import org.mule.api.transformer.TransformerException;
 import org.mule.transformer.AbstractMessageTransformer;
 
+import com.bhnetwork.integration.pppstodax.canonical.Division;
+import com.bhnetwork.integration.pppstodax.canonical.PartnerProfile;
+import com.microsoft.schemas.dynamics._2008._01.services.BhnDivision3PSServiceCreateRequest;
+
 public class CanonicalToBhnDivision3PSServiceCreateRequest extends
 		AbstractMessageTransformer {
 
 	@Override
 	public Object transformMessage(MuleMessage message, String outputEncoding)
 			throws TransformerException {
-		// TODO Auto-generated method stub
-		return null;
+		Division division = ((PartnerProfile) message.getPayload()).getCompany().getCpDivision().get(0);
+		BhnDivision3PSServiceCreateRequest req = new BhnDivision3PSServiceCreateRequest();
+		
+		return req;
 	}
 }
--- src/main/java/com/bhnetwork/integration/transformer/CanonicalToBhnUPCAttributes3PSServiceCreateRequest.java
+++ src/main/java/com/bhnetwork/integration/transformer/CanonicalToBhnUPCAttributes3PSServiceCreateRequest.java
@@ -4,13 +4,42 @@ import org.mule.api.MuleMessage;
 import org.mule.api.transformer.TransformerException;
 import org.mule.transformer.AbstractMessageTransformer;
 
+import com.bhnetwork.integration.pppstodax.canonical.PartnerProfile;
+import com.bhnetwork.integration.pppstodax.canonical.Product;
+import com.microsoft.schemas.dynamics._2008._01.documents.bhnupcattributes3ps.AxdEntityBhnUPCAttr1;
+import com.microsoft.schemas.dynamics._2008._01.documents.bhnupcattributes3ps.AxdEntityBhnUPCAttr2;
+import com.microsoft.schemas.dynamics._2008._01.documents.bhnupcattributes3ps.AxdEntityInventTable;
+import com.microsoft.schemas.dynamics._2008._01.documents.bhnupcattributes3ps.AxdEnumNoYes;
+import com.microsoft.schemas.dynamics._2008._01.documents.bhnupcattributes3ps.AxdbhnUPCAttributes3PS;
+import com.microsoft.schemas.dynamics._2008._01.services.BhnUPCAttributes3PSServiceCreateRequest;
+
 public class CanonicalToBhnUPCAttributes3PSServiceCreateRequest extends
 		AbstractMessageTransformer {
 
 	@Override
 	public Object transformMessage(MuleMessage message, String outputEncoding)
 			throws TransformerException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Product product = ((PartnerProfile) message.getPayload()).getCompany().getCpDivision().get(0).getProducts().get(0);
+		BhnUPCAttributes3PSServiceCreateRequest req = new BhnUPCAttributes3PSServiceCreateRequest();
+		
+		AxdbhnUPCAttributes3PS bhnUPCAttributes3PS = new AxdbhnUPCAttributes3PS(); 
+		
+		AxdEntityInventTable inventTable = new AxdEntityInventTable();
+		inventTable.setBhnCompany(product.getCompanyCode());
+		inventTable.setBhnDivision(product.getDivisionCode());
+		
+	    AxdEntityBhnUPCAttr1 bhnUPCAttr1 = new AxdEntityBhnUPCAttr1();
+	    bhnUPCAttr1.setVariableIndicator(product.getIsProductDenominationVariable()?AxdEnumNoYes.YES : AxdEnumNoYes.NO);
+	    
+	    AxdEntityBhnUPCAttr2 bhnUPCAttr2 = new AxdEntityBhnUPCAttr2();
+		bhnUPCAttr2.setCustomtemplatepathURL(product.getCustomTemplatePathURL());//TODO Stephane Fix the mapping document
+	    bhnUPCAttr2.setProductIsActivationRequired(product.getIsActivationRequired()?AxdEnumNoYes.YES : AxdEnumNoYes.NO);
+		
+		inventTable.getBhnUPCAttr1().add(bhnUPCAttr1);
+		inventTable.getBhnUPCAttr2().add(bhnUPCAttr2);
+		bhnUPCAttributes3PS.getInventTable().add(inventTable);
+		req.setBhnUPCAttributes3PS(bhnUPCAttributes3PS);
+		return req;
 	}
 }
