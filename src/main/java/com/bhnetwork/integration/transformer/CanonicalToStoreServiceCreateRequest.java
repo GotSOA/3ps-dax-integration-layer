a9773e18e6d8456f760131e1aac9d27c9b0e1fd6
Stephane Rata <stephane.rata@bhnetwork.com>
2014-06-05 22:50:20 -0700

    vendor and customerStore services fully functional - here and there a few mappings to clarify with team and at QA time. Demo ready for steps 6 & 7 in cascade.

--- src/main/java/com/bhnetwork/integration/transformer/CanonicalToStoreServiceCreateRequest.java
+++ src/main/java/com/bhnetwork/integration/transformer/CanonicalToStoreServiceCreateRequest.java
@@ -1,23 +1,15 @@
 package com.bhnetwork.integration.transformer;
 
 import java.text.ParseException;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.List;
 
-import javax.xml.datatype.DatatypeConfigurationException;
-import javax.xml.datatype.DatatypeConstants;
-import javax.xml.datatype.DatatypeFactory;
-import javax.xml.datatype.XMLGregorianCalendar;
+
 
 import org.mule.api.MuleMessage;
 import org.mule.api.transformer.TransformerException;
-import org.mule.api.transport.PropertyScope;
 import org.mule.transformer.AbstractMessageTransformer;
 
 import com.bhnetwork.integration.pppstodax.canonical.Store;
-import com.bhnetwork.integration.pppstodax.canonical.Vendor;
 import com.bhnetwork.integration.pppstodax.canonical.PartnerProfile;
 
 import com.microsoft.schemas.dynamics._2008._01.documents.customer.AxdEntityCustTable;
@@ -25,6 +17,7 @@ import com.microsoft.schemas.dynamics._2008._01.documents.customer.AxdArrayAxdEx
 import com.microsoft.schemas.dynamics._2008._01.documents.customer.AxdCustomer;
 import com.microsoft.schemas.dynamics._2008._01.documents.customer.AxdEntityAddressRelationship;
 import com.microsoft.schemas.dynamics._2008._01.documents.customer.AxdEntityCustAddress;
+import com.microsoft.schemas.dynamics._2008._01.documents.customer.AxdEnumDirPartyType;
 import com.microsoft.schemas.dynamics._2008._01.documents.customer.AxdExtTypeBhnCustomerLevel;
 import com.microsoft.schemas.dynamics._2008._01.documents.customer.AxdExtTypeCustBlocked;
 
@@ -41,71 +34,81 @@ public class CanonicalToStoreServiceCreateRequest extends
 		// see successful sample SOAP request to create a new Store
 		
 		List<Store> storeObj = ((PartnerProfile) message.getPayload()).getCompany().getCpDivision().get(0).getStores();
-		// check list size
-		// iterate through list
+		System.out.println("Detected " + storeObj.size() + " Store(s)");
 		
 		CustomerServiceCreateRequest req = new CustomerServiceCreateRequest();
 	
-		//child object
-		AxdEntityCustTable bhnStoreTable1 = new AxdEntityCustTable();
-		
 		//parent object
-		AxdCustomer storeTable = new AxdCustomer();
-		// storeTable is a List<AxdEntityCustTable>
+		AxdCustomer customerStore = new AxdCustomer();
+		// customerStore is a List<AxdEntityCustTable>
 		
-		//XML Attribute required
-		bhnStoreTable1.setClazz("entity");
-				
-		// List of fields to map
-//		private	String	companyPhysicalStoreAddressLine1;
-//		private	String	companyPhysicalStoreCountry;
-//		private	String	productLanguage;
-//		private	Boolean	storeBlocked;
-//		private	String	storeBHNCustLevel;
-//		private	String	storeBHNStoreCode;
-//		private	String	storeChannelDimension;
-//		private	String	storeContactPhoneNumber;
-//		private	String	storeCountryRegionId;
-//		private	String	storeCustGroup;
-//		private	String	storeDimensionPAC;
-//		private	String	storeDimensionRegion;
-//		private	String	storePhysicalStoreAddressLine1;
-//		private	String	storePhysicalStoreAddressLine2;
-//		private	String	storePhysicalStoreCity;
-//		private	String	storePhysicalStoreName;
-//		private	String	storePhysicalStorePostalCode;
-//		private	String	storePhysicalStoreState;
-//		private	String	storePriceToleranceGroup;
+		// iterate through list
+		for (Store curStore : storeObj) {
 		
-		// TODO: replace the get(0) by get(ii) while iterating
-		bhnStoreTable1.setAddress(storeObj.get(0).getStorePhysicalStoreAddressLine1() + " " + storeObj.get(0).getStorePhysicalStoreAddressLine2());
-		//companyPhysicalStoreCountry ??
-		bhnStoreTable1.setLanguageId(storeObj.get(0).getProductLanguage());
-		bhnStoreTable1.setBlocked(storeObj.get(0).getStoreBlocked()==false? AxdExtTypeCustBlocked.NO:AxdExtTypeCustBlocked.INVOICE );
-		bhnStoreTable1.setBhnCustLevel(storeObj.get(0).getStoreBHNCustLevel()==null? AxdExtTypeBhnCustomerLevel.NO_LEVEL:AxdExtTypeBhnCustomerLevel.STORE);
-		bhnStoreTable1.setBhnStoreCode(storeObj.get(0).getStoreBHNStoreCode());
-		//storeChannelDimension ??
-		bhnStoreTable1.setPhone(storeObj.get(0).getStoreContactPhoneNumber());
-		bhnStoreTable1.setCountryRegionId(storeObj.get(0).getCompanyPhysicalStoreCountry());
-		//storeCustGroup ??
-		// TODO: fix issue below
-		//bhnStoreTable1.setDimension(storeObj.get(0).getStoreChannelDimension());		// AxdArrayAxdExtTypeDimension
-		// storeDimensionRegion ??
-		bhnStoreTable1.setCity(storeObj.get(0).getStorePhysicalStoreCity());
-		// storePhysicalStoreName ??
-		bhnStoreTable1.setName(storeObj.get(0).getStorePhysicalStoreName());
-		bhnStoreTable1.setZipCode(storeObj.get(0).getStorePhysicalStorePostalCode());
-		bhnStoreTable1.setState(storeObj.get(0).getStorePhysicalStoreState());
-		bhnStoreTable1.setProjPriceGroup(storeObj.get(0).getStorePriceToleranceGroup());
+			//child object
+			AxdEntityCustTable bhncustomerStoreTable1 = new AxdEntityCustTable();
 		
-		
-		
-
-
-		 
-		storeTable.getCustTable().add(bhnStoreTable1);
-		req.setCustomer(storeTable);
-		
+			//XML Attribute required
+			bhncustomerStoreTable1.setClazz("entity");
+					
+			// List of fields to map
+	//		private	String	companyPhysicalStoreAddressLine1;
+	//		private	String	companyPhysicalStoreCountry;
+	//		private	String	productLanguage;
+	//		private	Boolean	storeBlocked;
+	//		private	String	storeBHNCustLevel;
+	//		private	String	storeBHNStoreCode;
+	//		private	String	storeChannelDimension;
+	//		private	String	storeContactPhoneNumber;
+	//		private	String	storeCountryRegionId;
+	//		private	String	storeCustGroup;
+	//		private	String	storeDimensionPAC;
+	//		private	String	storeDimensionRegion;
+	//		private	String	storePhysicalStoreAddressLine1;
+	//		private	String	storePhysicalStoreAddressLine2;
+	//		private	String	storePhysicalStoreCity;
+	//		private	String	storePhysicalStoreName;
+	//		private	String	storePhysicalStorePostalCode;
+	//		private	String	storePhysicalStoreState;
+	//		private	String	storePriceToleranceGroup;
+			
+			// TODO: replace the get(0) by get(ii) while iterating
+			bhncustomerStoreTable1.setAddress(curStore.getStorePhysicalStoreAddressLine1() + " " + curStore.getStorePhysicalStoreAddressLine2());
+			//companyPhysicalStoreCountry ??
+			bhncustomerStoreTable1.setLanguageId(curStore.getProductLanguage());
+			bhncustomerStoreTable1.setBlocked(curStore.getStoreBlocked()==false? AxdExtTypeCustBlocked.NO:AxdExtTypeCustBlocked.INVOICE );
+			// issue when setting these 2
+			//bhncustomerStoreTable1.setBhnCompanyCode("foobar");
+			//bhncustomerStoreTable1.setBhnDivisionCode("123");
+			
+			bhncustomerStoreTable1.setBhnCustLevel(curStore.getStoreBHNCustLevel()==null? AxdExtTypeBhnCustomerLevel.NO_LEVEL:AxdExtTypeBhnCustomerLevel.STORE);
+			bhncustomerStoreTable1.setBhnStoreCode(curStore.getStoreBHNStoreCode());
+			//storeChannelDimension ??
+			// Currency MISSING in JSON, for now defaulting to USD
+			bhncustomerStoreTable1.setCurrency("USD");
+			// CustGroup MISSING in JSON
+			bhncustomerStoreTable1.setCustGroup(curStore.getStoreCustGroup());
+			// bhncustomerStoreTable1.setCustItemGroupId(null);
+			// TODO: PartyType MISSING in canonical
+			// bhncustomerStoreTable1.setPartyType(isnull(curStore.getPartyType())? AxdEnumDirPartyType.PERSON:AxdEnumDirPartyType.ORGANIZATION);
+			bhncustomerStoreTable1.setPartyType(AxdEnumDirPartyType.PERSON);   // temporary HACK
+			bhncustomerStoreTable1.setPaymDayId(null);
+			bhncustomerStoreTable1.setPhone(curStore.getStoreContactPhoneNumber());
+			bhncustomerStoreTable1.setCountryRegionId(curStore.getCompanyPhysicalStoreCountry());
+			//storeCustGroup ??
+			// TODO: fix issue below
+			//bhnStoreTable1.setDimension(curStore.getStoreChannelDimension());		// AxdArrayAxdExtTypeDimension
+			// storeDimensionRegion ??
+			bhncustomerStoreTable1.setCity(curStore.getStorePhysicalStoreCity());
+			// storePhysicalStoreName ??
+			bhncustomerStoreTable1.setName(curStore.getStorePhysicalStoreName());
+			bhncustomerStoreTable1.setZipCode(curStore.getStorePhysicalStorePostalCode());
+			bhncustomerStoreTable1.setState(curStore.getStorePhysicalStoreState());
+			bhncustomerStoreTable1.setProjPriceGroup(curStore.getStorePriceToleranceGroup());
+					 
+			customerStore.getCustTable().add(bhncustomerStoreTable1);
+			req.setCustomer(customerStore);
+		} // end for loop
 		return req;
 	}
 }
